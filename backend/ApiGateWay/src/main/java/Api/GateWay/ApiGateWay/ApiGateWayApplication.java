f66ad0759f3a1d6b19a8fea68eb0757896f5e74d
Hiddennnn <91212536+MrHiddennnn@users.noreply.github.com>
2024-01-05 10:36:14 -0500

    BACKEND GATEWAY

---
+++ backend/ApiGateWay/src/main/java/Api/GateWay/ApiGateWay/ApiGateWayApplication.java
@@ -0,0 +1,13 @@
+package Api.GateWay.ApiGateWay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGateWayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGateWayApplication.class, args);
+	}
+
+}
